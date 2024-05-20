cdbf4dae8570560c8d87ce9fa8eaa0365864a59f
Yash Athwani <yashathwani45@gmail.com>
2024-05-21 04:24:55 +0530

    quiz app

---
+++ script.js
@@ -0,0 +1,85 @@
+let questions = [   
+    {
+        question: 'Which HTML tag is used to define an inline style?',
+        choice1: '<script>',
+        choice2: '<css>',
+        choice3: '<style>',
+        choice4: '<span>',
+        answer: 3,
+    },
+    {
+        question: 'Which property is used to change the text color in CSS?',
+        choice1: 'text-color',
+        choice2: 'font-color',
+        choice3: 'text-style',
+        choice4: 'color',
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is the correct way to comment in HTML?',
+        choice1: '// Comment',
+        choice2: '<!-- Comment -->',
+        choice3: '/* Comment */',
+        choice4: '<! Comment>',
+        answer: 2,
+    },
+ ];
+
+function randomize(array){
+    let current=array.length;
+    let random;
+    while(current!=0){
+        random=Math.floor(Math.random()*current);
+        current--;
+        let temp = array[current];
+        array[current] = array[random];
+        array[random] = temp;
+    }
+    return array;
+}
+let option1 = document.getElementsByClassName('optionend')[0];
+let option2 = document.getElementsByClassName('optionend')[1];
+let option3 = document.getElementsByClassName('optionend')[2];
+let option4 = document.getElementsByClassName('optionend')[3];
+let question = document.getElementsByClassName('question')[0];
+let currentQuestion = 0;
+let score = 0;
+let options = document.getElementsByClassName('option');
+questions = randomize(questions);
+let totalQuestions = questions.length;
+
+function loadQuestion(questionIndex) {
+    let q = questions[questionIndex];
+    question.innerText = q.question;
+    option1.innerText = q.choice1;
+    option2.innerText = q.choice2;
+    option3.innerText = q.choice3;
+    option4.innerText = q.choice4;
+}
+
+loadQuestion(currentQuestion);
+
+Array.from(options).forEach(option => {
+    option.addEventListener('click', () => {
+        currentQuestion++;
+        if (option.id == questions[currentQuestion-1].answer) {
+        option.style.backgroundColor = 'green';
+        }
+        else{
+            option.style.backgroundColor = 'red';
+        }
+        if (currentQuestion < totalQuestions) {
+            setTimeout(() =>{ loadQuestion(currentQuestion)
+            option.style.backgroundColor = 'white';
+            
+            }, 2000);
+            
+        }
+        else{
+            setTimeout(() =>{ 
+                window.location.href='end.html'
+            }, 2000);
+           
+        }
+    });
+});
